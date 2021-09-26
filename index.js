5cf8f7370c875b977b30b2f7002e39c652f00eaf
DeviousLab <deviouslab@gmail.com>
2021-09-26 22:44:22 +0400

    Added backend code
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+import { Router } from 'itty-router';
+import { customAlphabet } from 'nanoid';
+
+const router = Router();
+const nanoid = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  6,
+);
+
+router.post('/links', async request => {
+  let slug = nanoid();
+  let requestBody = await request.json();
+  if ('url' in requestBody) {
+    await SHORTEN.put(slug, requestBody.url, { expirationTtl: 86400 });
+    let shortenedURL = `${new URL(request.url).origin}/${slug}`;
+    let responseBody = {
+      message: 'Link shortened successfully',
+      slug,
+      shortened: shortenedURL,
+    };
+    return new Response(JSON.stringify(responseBody), {
+      headers: { 'content-type': 'application/json' },
+      status: 200,
+    });
+  } else {
+    return new Response("Must provide a valid URL", { status: 400 });
+  }
+});
+
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  event.respondWith(router.handle(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
-}
