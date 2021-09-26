ed40aa85bd0f0ba63713f3ebaa971b2affb4d4f7
DeviousLab <deviouslab@gmail.com>
2021-09-26 22:54:25 +0400

    Used kv-asset-handler to render application
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Router } from 'itty-router';
 import { customAlphabet } from 'nanoid';
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
 
 const router = Router();
 const nanoid = customAlphabet(
@@ -27,6 +28,15 @@ router.post('/links', async request => {
   }
 });
 
+async function handleEvent(event) {
+  let requestUrl = new URL(event.request.url);
+  if (requestUrl.pathname === '/' || requestUrl.pathname.includes('static')) {
+    return await getAssetFromKV(event);
+  } else {
+    return await router.handle(event.request);
+  }
+}
+
 addEventListener('fetch', event => {
-  event.respondWith(router.handle(event.request))
-})
+  event.respondWith(handleEvent(event));
+});
